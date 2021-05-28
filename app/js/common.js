adeea57249720b02924bb3cf4364787891a7eaed
Viktoria <zaharova181114@gmail.com>
2021-05-28 14:08:42 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,31 @@
+setTimeout(function () {
+    $(".preloader").delay(1000).fadeOut('show').remove();
+
+    $('.home').removeClass('home-default').addClass('home-load');
+}, 2000);
+
+
+
+$('[name="phone"]').mask('+7 | (999) 999-99-99');
+
+$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+    $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+});
+
+// accordeon
+function accordeon() {
+    var panel = $('.panel_heading');
+
+    if (panel.hasClass('in')) {
+        $('.in').find('.block_hover').slideDown();
+    }
+
+    $('.panel_heading .block_title').on('click', function () {
+        $(this).parent().toggleClass('in').find('.block_hover').slideToggle();
+    });
+}
+
+accordeon();
+
